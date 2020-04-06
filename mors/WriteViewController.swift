feb32c7785ec4933ef31452264d922dee98124f2
Bradley Bottomlee <bradley.bttm@gmail.com>
2020-04-05 19:06:36 -0500

    Initial commit

---
+++ mors/WriteViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WriteViewController.swift
+//  mors
+//
+//  Created by Bradley Bottomlee on 4/5/20.
+//  Copyright © 2020 Bradley Bottomlee. All rights reserved.
+//
+
+import UIKit
+
+class WriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
